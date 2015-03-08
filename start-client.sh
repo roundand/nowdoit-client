b90d9c0d371de65d3604273c05b2a1e81d59fac7
Francis Norton <francis.norton@gmail.com>
2015-03-08 12:21:08 +0000

    devops demo

--- start-client.sh
+++ start-client.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+docker build -t nginx_img_1 .
 docker run --name client -d -p 80:80 -p 8080:8080 nginx_img_1
 sleep 0.5
 curl -GET 127.0.0.1:8080/index.html
--- start-repo.sh
+++ start-repo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+docker  pull roundand/nowdoit-client
 docker run --name repo -d -p 80:80 -p 8080:8080 roundand/nowdoit-client
 sleep 0.5
 curl -GET 127.0.0.1:8080/index.html
